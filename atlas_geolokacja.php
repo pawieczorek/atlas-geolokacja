1978ebc0db030cbb5be5c0c167f357d8bd1cd459
pawieczorek <piotrwieczorek@wp.eu>
2017-01-13 19:35:03 +0100

    Update atlas_geolokacja.php

--- atlas_geolokacja.php
+++ atlas_geolokacja.php
@@ -1,11 +1,11 @@
 <?php
 
-//poniższ aplikacja integruje dane demograficzne (miasto, populacja) z danymi geolokacyjnymi
+//poniższa aplikacja integruje dane demograficzne (miasto, populacja) z danymi geolokacyjnymi
 //(szerość i długość geograficzna)
 //dane demograficzne znajdują się w bazie mysql w przykładowej tabeli citiespoland
 //kulumny: miasto (text), populacja(int)
 //dane geolokalizacyjne pochodzą z Google Maps Geocoding API i są odpowiednio umieszczane
-//w kolumnach lat(decimal(10,8)), lng(decimal 11,8)) 
+//w kolumnach lat(decimal(10,8)), lng(decimal (11,8)) 
 //przebieg wykonywania kodu jest spowolniony przez komendę usleep (100000); ze względu
 //limit sekundowy zapytań do Google Maps Geocoding API
 //
